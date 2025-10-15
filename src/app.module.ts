bc51415c8e7b445a40f5e8983d868f0ee5b4e267
Artur Ceschin <artur.ceschin@gmail.com>
2025-10-15 10:24:34 -0300

    feat: sign in flow with env validations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule],
+  imports: [DatabaseModule, UsersModule, AuthModule],
 })
 export class AppModule {}
