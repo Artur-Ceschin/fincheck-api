2284c5928eab9a89f4761cdcd0bce5ddaf4ababf
Artur Ceschin <artur.ceschin@gmail.com>
2025-10-15 16:20:44 -0300

    feat: user refactor and usage

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthenticateDto } from './dto/authenticate.dto';
+import { SignUpDto } from './dto/signup-dto';
+import { SignInDto } from './dto/singin-dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('sign-in')
-  authenticate(@Body() authenticateDto: AuthenticateDto) {
-    return this.authService.authenticate(authenticateDto);
+  @Post('signin')
+  signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signin(signInDto);
+  }
+
+  @Post('signup')
+  signUp(@Body() signUpDto: SignUpDto) {
+    return this.authService.signup(signUpDto);
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,8 +1,13 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { AuthenticateDto } from './dto/authenticate.dto';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
-import { compare } from 'bcryptjs';
+import { compare, hash } from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
+import { SignUpDto } from './dto/signup-dto';
+import { SignInDto } from './dto/singin-dto';
 
 @Injectable()
 export class AuthService {
@@ -11,8 +16,8 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
 
-  async authenticate(authenticateDto: AuthenticateDto) {
-    const { email, password } = authenticateDto;
+  async signin(signInDto: SignInDto) {
+    const { email, password } = signInDto;
 
     const user = await this.usersRepository.findUnique({
       where: { email },
@@ -28,10 +33,122 @@ export class AuthService {
       throw new UnauthorizedException('Invalid credentials');
     }
 
-    const accessToken = this.jwtService.sign({ sub: user.id });
+    const accessToken = await this.generateAccessToken(user.id);
 
     return {
       accessToken,
     };
+  }
+
+  async signup(createUserDto: SignUpDto) {
+    const { name, email, password } = createUserDto;
+    const emailTaken = await this.usersRepository.findUnique({
+      where: { email },
+      select: {
+        id: true,
+      },
+    });
+
+    if (emailTaken) {
+      throw new ConflictException('Email already taken');
+    }
+
+    const hashedPassword = await hash(password, 12);
+
+    const user = await this.usersRepository.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        categories: {
+          createMany: {
+            data: [
+              {
+                name: 'Salary',
+                icon: 'travel',
+                type: 'INCOME',
+                color: '#00AA63',
+              },
+              {
+                name: 'Freelance',
+                icon: 'freelance',
+                type: 'INCOME',
+                color: '#0084F4',
+              },
+              {
+                name: 'Other',
+                icon: 'other',
+                type: 'INCOME',
+                color: '#6B7280',
+              },
+              {
+                name: 'Home',
+                icon: 'home',
+                type: 'EXPENSE',
+                color: '#F97316',
+              },
+              {
+                name: 'Food',
+                icon: 'food',
+                type: 'EXPENSE',
+                color: '#EF4444',
+              },
+              {
+                name: 'Education',
+                icon: 'education',
+                type: 'EXPENSE',
+                color: '#8B5CF6',
+              },
+              {
+                name: 'Leisure',
+                icon: 'fun',
+                type: 'EXPENSE',
+                color: '#EC4899',
+              },
+              {
+                name: 'Grocery',
+                icon: 'grocery',
+                type: 'EXPENSE',
+                color: '#10B981',
+              },
+              {
+                name: 'Clothes',
+                icon: 'clothes',
+                type: 'EXPENSE',
+                color: '#06B6D4',
+              },
+              {
+                name: 'Transport',
+                icon: 'transport',
+                type: 'EXPENSE',
+                color: '#F59E0B',
+              },
+              {
+                name: 'Travel',
+                icon: 'travel',
+                type: 'EXPENSE',
+                color: '#3B82F6',
+              },
+              {
+                name: 'Other',
+                icon: 'other',
+                type: 'EXPENSE',
+                color: '#6B7280',
+              },
+            ],
+          },
+        },
+      },
+    });
+
+    const accessToken = await this.generateAccessToken(user.id);
+
+    return {
+      accessToken,
+    };
+  }
+
+  private async generateAccessToken(userId: string) {
+    return this.jwtService.sign({ sub: userId });
   }
 }
